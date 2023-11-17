b55646d271665846f9cddd3d7190f14d260cab23
rana ammar <ranaammar344@gmail.com>
2023-11-18 00:02:11 +0500

    fsdffas

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -23,9 +23,7 @@ export default function Home() {
     </p>
   </main>
 
-  <footer class="mt-auto text-white-50">
-    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
-  </footer>
+  
 </div>
     </div>
     <div className='contianer my-5 mx-4'>
